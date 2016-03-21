0d390b7ebdb3843eddcaf8f89bd313f145230296
Daniel Smith <Daniel.Smith.ITguy@gmail.com>
2016-03-21 17:08:06 -0600

    Adding untracked files to master

---
+++ db/migrate/20160320234339_create_cars.rb
@@ -0,0 +1,13 @@
+class CreateCars < ActiveRecord::Migration
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.integer :year
+      t.decimal :price
+      t.decimal :price, :precision => 8, :scale => 2
+
+      t.timestamps null: false
+    end
+  end
+end
